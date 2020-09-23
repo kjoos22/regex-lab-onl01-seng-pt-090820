4059a80effe2a25a753951c60441fda1e9479c52
Kristian Joos <kjoos22@gmail.com>
2020-09-23 19:13:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   words = []
   text = text.split(" ")
   text.each do |word|
-    if text.match(/\A(un)/) and text.match(/\z(ing)/)
+    if word.match(/\A(un)/) and word.match(/\z(ing)/)
       words << word
     end
   end
