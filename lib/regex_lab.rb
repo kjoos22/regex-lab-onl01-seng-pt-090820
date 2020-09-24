4012d9a108ab15bd91f53e831fe4704e20fe9432
Kristian Joos <kjoos22@gmail.com>
2020-09-24 00:37:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -31,7 +31,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)
-    if text[-1].match(/./)
+    if text[-1].match(/[.]/)
       return true
     end
   end
