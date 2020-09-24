07ad9f25393fbe0ec2e3c26242dc2378f9d0c34e
Kristian Joos <kjoos22@gmail.com>
2020-09-24 00:27:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -30,8 +30,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A(ABCDEFGHIJKLMNOPQRSTUVWXYZ)/)
-    if text.match(/\z[.]/)
+  if text.match(/\A[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)
+    if text.match(/\z ./)
       return true
     end
   end
